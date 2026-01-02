366b519b8ac786c460b87c9d4ccdaadfba01cd89
Jonathan White <jawh@umich.edu>
2026-01-01 18:52:19 -0700

    Auto generated unit tests of provided multiply libraries.

---
+++ test/multiply/MultiplyTest.hpp
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <gtest/gtest.h>
+
+#include <array>
+#include <random>
+
+class MultiplyTest : public testing::Test {
+ protected:
+  static constexpr auto N = 1 << 10;
+  std::array<double, N> a{};
+  std::array<double, N> b{};
+  std::array<double, N> c{};
+
+  MultiplyTest() {
+    auto gen = std::mt19937{};
+    auto dist = std::uniform_real_distribution<double>{0.0, 1.0};
+    for (auto& x : a) {
+      x = dist(gen);
+    }
+    for (auto& x : b) {
+      x = dist(gen);
+    }
+  }
+
+  template <auto multiplier>
+  void multiply() {
+    multiplier(N, a.data(), b.data(), c.data());
+  }
+};
--- test/multiply/multiply_test.cpp
+++ test/multiply/multiply_test.cpp
@@ -1,28 +1,6 @@
 #include "multiply/multiply.hpp"
 
-#include <gtest/gtest.h>
-
-#include <array>
-#include <random>
-
-class MultiplyTest : public testing::Test {
- protected:
-  static constexpr auto N = 1 << 10;
-  std::array<double, N> a{};
-  std::array<double, N> b{};
-  std::array<double, N> c{};
-
-  MultiplyTest() {
-    auto gen = std::mt19937{};
-    auto dist = std::uniform_real_distribution<double>{0.0, 1.0};
-    for (auto& x : a) {
-      x = dist(gen);
-    }
-    for (auto& x : b) {
-      x = dist(gen);
-    }
-  }
-};
+#include "MultiplyTest.hpp"
 
 TEST_F(MultiplyTest, SingleThreadedMultiplyTest) {
   single_threaded_multiply(N, a.data(), b.data(), c.data());
