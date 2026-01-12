14d5f560961ebe7b32adcef5dfead35de9c405f4
Jonathan White <jawh@umich.edu>
2026-01-11 22:26:42 -0700

    Created object files for SingleThreadedExecutor_multiply and ThreadPoolExecutor_multiply.

--- src/multiply/SingleThreadedExecutor_multiply.cpp
+++ src/multiply/SingleThreadedExecutor_multiply.cpp
@@ -1,9 +1,8 @@
 #include "ParallelExecutor/SingleThreadedExecutor.hpp"
-#include "multiply/multiply.hpp"
 #include "multiply/par_exec_multiply.hpp"
 
-void SingleThreadedExecutor_multiply(std::size_t n, const double *a,
+void SingleThreadedExecutor_multiply(ParODE::SingleThreadedExecutor &exe,
+                                     std::size_t n, const double *a,
                                      const double *b, double *c) {
-  static auto exe = ParODE::SingleThreadedExecutor{};
   par_exec_multiply(exe, n, a, b, c);
 }
