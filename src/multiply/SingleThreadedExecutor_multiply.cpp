e4c12b3236210a8eae881436b1f002379b371805
Jonathan White <jawh@umich.edu>
2026-01-17 16:25:25 -0700

    Removed unused source files.

--- src/multiply/SingleThreadedExecutor_multiply.cpp
+++
@@ -1,8 +0,0 @@
-#include "ParallelExecutor/SingleThreadedExecutor.hpp"
-#include "multiply/par_exec_multiply.hpp"
-
-void SingleThreadedExecutor_multiply(ParODE::SingleThreadedExecutor &exe,
-                                     std::size_t n, const double *a,
-                                     const double *b, double *c) {
-  par_exec_multiply(exe, n, a, b, c);
-}
