97d7c8a7f2c185899a805c2513d34b8624ad2a55
Jonathan White <jawh@umich.edu>
2026-01-17 12:32:55 -0700

    Removed redundant ParExec benchmarks.

--- benchmark/multiply/SingleThreadedExecutor_multiply_benchmark.cpp
+++
@@ -1,5 +0,0 @@
-#include "BM_Multiply.hpp"
-#include "ParExecMultiplyBenchmarker.hpp"
-#include "ParallelExecutor/SingleThreadedExecutor.hpp"
-
-BM_MULTIPLY_SET(ParExecMultiplyBenchmarker<ParODE::SingleThreadedExecutor>);
