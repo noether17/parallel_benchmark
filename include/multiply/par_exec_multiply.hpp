84641938b5b85cd45a5c54d09e9535c5651c3aef
Jonathan White <jawh@umich.edu>
2026-01-17 21:25:18 -0700

    Fixed bug that prevented ParExec from using AVX instructions.

--- include/multiply/par_exec_multiply.hpp
+++ include/multiply/par_exec_multiply.hpp
@@ -2,12 +2,12 @@
 
 #include "ParallelExecutor/ParallelExecutor.hpp"
 
-inline constexpr void multiply_kernel(int i, double const* a, double const* b,
-                                      double* c) {
+inline constexpr void multiply_kernel(std::size_t i, double const* a,
+                                      double const* b, double* c) {
   c[i] = a[i] * b[i];
 }
 template <typename ParallelExecutor>
-void par_exec_multiply(ParallelExecutor& par_exec, int n, double const* a,
-                       double const* b, double* c) {
+void par_exec_multiply(ParallelExecutor& par_exec, std::size_t n,
+                       double const* a, double const* b, double* c) {
   ParODE::call_kernel<multiply_kernel>(par_exec, n, a, b, c);
 }
